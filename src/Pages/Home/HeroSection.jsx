8f5d93d2f22dea7e28f854769bfb8b263c0effbb
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-16 18:26:23 -0800

    framework in place for hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,10 +3,22 @@ export default function HeroSection() {
     <section id="hereSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section-content">
-          <p className="section--title">
-            Hey, I'm Be-Asia
+          <p className="section--title">Hey, I'm Be-Asia </p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br />
+            Lorem ipsum dolor sit amet elit?
           </p>
         </div>
+        <button className="btn btn-primary">Get In Touch!</button>
+      </div>
+      <div className="hero--section--image">
+        <img src="./img/hero_img.png" alt="Hero Section"/>
       </div>
     </section>
   );
