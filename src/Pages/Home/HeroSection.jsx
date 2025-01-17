10271fa9c6eaac6222662bdecc2e64493aca2daf
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-16 18:18:42 -0800

    build out divs for hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,13 @@
 export default function HeroSection() {
-  return <h1>Hero Section</h1>;
+  return (
+    <section id="hereSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section-content">
+          <p className="section--title">
+            Hey, I'm Be-Asia
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
