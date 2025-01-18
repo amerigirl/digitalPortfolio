54738612c451e7222ae260b0abd1dcad9da2e502
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-17 23:11:19 -0800

    troubleshooting image

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <section id="hereSection" className="hero--section">
       <div className="hero--section--content--box">
-        <div className="hero--section-content">
+        <div className="hero--section--content">
           <p className="section--title">Hey, I'm Be-Asia </p>
           <h1 className="hero--section--title">
             <span className="hero--section-title--color">Full Stack</span>{" "}
@@ -10,9 +10,7 @@ export default function HeroSection() {
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            <br />
-            Lorem ipsum dolor sit amet elit?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet elit?
           </p>
         </div>
         <button className="btn btn-primary">Let's Chat!</button>
