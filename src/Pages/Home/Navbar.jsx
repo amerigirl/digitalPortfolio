ccef74249d091227465a6955b73b33e46f00fdaa
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-01 12:53:52 -0800

    adding navbar and connecting it via routes

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect, use} from 'react';
+import { Link } from "react-scroll";
+
+export default function Navbar(){
+const [navActive, setNavActive] = useState(false);
+
+const toggleNav = () => {
+    setNavActive((navActive) => !navActive);
+}
+
+const closeMenu = () => {
+    setNavActive(false);
+}
+
+useEffect(() =>{
+    const handleResize = () => {
+        if(window.innerWidth <= 500){
+            closeMenu();
+        }
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    }
+}, []);
+
+useEffect(() => {
+    if(window.innerWidth <= 1200){
+        closeMenu();
+    }
+}, []);
+
+return (
+    <nav className={`navbar ${navActive ? 'active' : ''}`}>
+<div>
+    <img src='./img/logo.svg' alt='logo' />
+</div>
+    </nav>
+)
+}
