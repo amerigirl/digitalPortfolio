8656001abcb1d55a6130d07dfd99dee8ae08d7ef
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-02 11:27:51 -0800

    flushing out the about me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section--description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+            quas consectetur nulla laboriosam unde. Minus explicabo mollitia qui
+            veniam? Porro blanditiis et labore illo saepe officia iusto sequi
+            ducimus autem!
+          </p>
+          <p className="hero--section--description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+            quas consectetur nulla laboriosam unde. Minus explicabo mollitia qui
+        
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,3 +1,4 @@
+import AboutMe from '../AboutMe';
 import HeroSection from './HeroSection/HeroSection';
 import MySkills from './MySkills/MySkills';
 
@@ -6,6 +7,7 @@ export default function Home(){
       <>
         <HeroSection />
         <MySkills />
+        <AboutMe />
       </>
     );
 }
