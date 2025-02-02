b5d6c7a1cb70b5a4bf5f41568761d627a75dd594
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-02 15:33:55 -0800

    updated link connections

--- src/Pages/HeroSection/HeroSection.jsx
+++ src/Pages/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import './HeroSection.css';
 
 export default function HeroSection() {
   return (
-    <section id="hereSection" className="hero--section">
+    <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Be-Asia </p>
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -66,10 +66,10 @@ export default function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
-              to="MyPortfolio"
+              to="AboutMe"
               className="navbar--content"
             >
-              Portfolio
+              About Me
             </Link>
           </li>
           <li>
@@ -80,10 +80,10 @@ export default function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
-              to="AboutMe"
+              to="mySkills"
               className="navbar--content"
             >
-              About Me
+              Skills
             </Link>
           </li>
           <li>
