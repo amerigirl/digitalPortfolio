0866b7ccb95379aaf2bcef38d97ea59e9737a462
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-02 10:49:04 -0800

    mock skills completed

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -13,6 +13,15 @@ export default function MySkills() {
             <div className="skills--section--img">
               <img src={item.src} alt="Product Chain" />
             </div>
+            <div className="skills--section--card">
+              <h3 className="skills--section--title">
+                {item.title}
+                </h3>
+                <p className="skills--section--description">
+                  {item.description}
+                </p>
+              
+            </div>
           </div>
         ))}
       </div>
