41cf91b1af1c3b666f81627ac2445002d73b43f2
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-02 11:14:56 -0800

    skills css complete

--- src/Pages/Home/Homescreen/MySkills/MySkills.jsx
+++ src/Pages/Home/Homescreen/MySkills/MySkills.jsx
@@ -14,7 +14,8 @@ export default function MySkills() {
             <div className="skills--section--img">
               <img src={item.src} alt="Product Chain" />
             </div>
-            <div className="skills--section--card">
+
+            <div className="skills--section--card--content">
               <h3 className="skills--section--title">
                 {item.title}
                 </h3>
