f4e67cdeafb42e8aa8c45c10e0c1afd927db9032
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-02 16:23:08 -0800

    working on viewing images

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,13 +1,15 @@
 import HeroSection from '../../HeroSection/HeroSection';
 import MySkills from '../../MySkills/MySkills';
 import AboutMe from '../../AboutMe/AboutMe';
+import MyPortfolio from '../../MyPortfolio/MyPortfolio';
+
 export default function Home(){
     return (
       <>
         <HeroSection />
-          <AboutMe />
+        <AboutMe />
         <MySkills />
-      
+        <MyPortfolio />
       </>
     );
 }
